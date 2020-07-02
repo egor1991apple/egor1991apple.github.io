fce1f249d32188b2936c5d7125d1aa9893e16d81
egor1991apple <egor1991apple@gmail.com>
2020-07-02 11:20:30 +0300

    update
    123

---
+++ component---src-pages-page-2-js-44621ff85d8c7a114ed0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{p5nM:function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),o=t.n(n),c=t("Wbzz"),p=t("L12J"),l=t("vrFN");a.default=function(){return o.a.createElement(p.a,{pageInfo:{pageName:"page-2"}},o.a.createElement(l.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-44621ff85d8c7a114ed0.js.map
