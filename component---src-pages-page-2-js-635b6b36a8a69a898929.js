5be2c21e9883fca25259a0b1a3232dc056fab13a
egor1991apple <egor1991apple@gmail.com>
2020-07-14 11:40:10 +0300

    udapte

--- component---src-pages-page-2-js-635b6b36a8a69a898929.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{p5nM:function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),o=t.n(n),c=t("Wbzz"),p=t("L12J"),l=t("vrFN");a.default=function(){return o.a.createElement(p.a,{pageInfo:{pageName:"page-2"}},o.a.createElement(l.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-635b6b36a8a69a898929.js.map
